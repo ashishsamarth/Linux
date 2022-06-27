8a67f6073516eae131a55eed25e2f6c0378bdee6
Ashish Samarth <mrashishsamarth@gmail.com>
2022-06-27 14:58:03 -0500

    get_machine_id

---
+++ Fedora/bash-functions/get_machine_id.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Declare the function name
+get_machine_id()
+{
+	# Always make sure read is present
+	# read allows to run and return the output of the command to the variable
+	# xargs removes the leading and trailing spaces if any in the output
+	my_machine_id=read hostnamectl|grep -i machine|cut -d ':' -f2|xargs
+	# return value of the function is machine id
+	return $my_machine_id
+}
+
+# Calling the function
+get_machine_id
