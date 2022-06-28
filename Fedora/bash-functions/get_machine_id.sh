6592a502359cd16704ba47f3551637d7911df641
Ashish Samarth <mrashishsamarth@gmail.com>
2022-06-28 08:19:12 -0500

    get_machine_id

--- Fedora/bash-functions/get_machine_id.sh
+++ Fedora/bash-functions/get_machine_id.sh
@@ -7,6 +7,10 @@ get_machine_id()
 	# read allows to run and return the output of the command to the variable
 	# xargs removes the leading and trailing spaces if any in the output
 	my_machine_id=read hostnamectl|grep -i machine|cut -d ':' -f2|xargs
+
+	# Another way of executing the piped command in bash is by using command keyword
+	#my_machine_id=command hostnamectl|grep -i machine|cut -d ':' -f2|xargs
+
 	# return value of the function is machine id
 	return $my_machine_id
 }
