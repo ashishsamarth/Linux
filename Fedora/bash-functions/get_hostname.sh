6a964812d338c1a843a66660b5858ea0e5d2f3f2
Ashish Samarth <mrashishsamarth@gmail.com>
2022-06-28 08:18:58 -0500

    get_hostname

--- Fedora/bash-functions/get_hostname.sh
+++ Fedora/bash-functions/get_hostname.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# declare the function name
+# Declare the function name
 get_hostname()
 {
     # Always make sure that read is present
     # read allows to run and return the output of the command to the variable
     # xargs removes the leading the trailing spaces if any in the output
 	my_hostname=read hostnamectl|grep -i hostname|cut -d ':' -f2|xargs
-    # return value of the function is hostname
+    	
+	# Another way of executing the piped command in bash is by using command keyword
+	# my_hostname=command hostnamectl|grep -i hostname|cut -d ':' -f2|xargs
+	
+	# return value of the function is hostname
 	return $my_hostname
 }
 
