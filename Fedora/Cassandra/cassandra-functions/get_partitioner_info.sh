761b1778f07e6411822489e84f188f74ced59f22
Ashish Samarth <mrashishsamarth@gmail.com>
2022-07-01 08:48:02 -0500

    get_configured_partitioner

---
+++ Fedora/Cassandra/cassandra-functions/get_partitioner_info.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# Declare the function name
+get_configured_partitioner()
+{	
+	# Positional Arguement of node number is assiged to variable
+	node_num=$1
+	# Always make sure read is present
+	# read allows to run and return the output of the command to the variable
+	# final output is the configured-partitioner of the node number provided by user as cmd line argument
+	conf_partitioner=read grep -i ^partitioner ~/node$node_num/resources/cassandra/conf/cassandra.yaml | rev | cut -d '.' -f1 | rev
+	
+	# Return value of the function is the configured partitioner for given node
+	return $conf_partitioner
+}
+
+# Calling the function
+get_configured_partitioner $1
