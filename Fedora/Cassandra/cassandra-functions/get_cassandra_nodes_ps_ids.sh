ec7ccbb864e5761e57514c459d1a9ebeba409d49
Ashish Samarth <mrashishsamarth@gmail.com>
2022-06-30 21:54:24 -0500

    get_cassandra_nodes_ps_ids

---
+++ Fedora/Cassandra/cassandra-functions/get_cassandra_nodes_ps_ids.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+# Declare function name
+get_cassandra_ps_id_by_node_num()
+{
+	# Positional Arguement of node number is assiged to variable
+	node_num=$1
+	# Always make sure read is present
+	# read allows to run and return the output of the command to the variable
+	# $2 gets the pid for the node
+	# $50 helps identifies the node number
+	# final output is the process id of the node number provided by user as cmd line argument
+	my_pid=read ps -aux | grep Ddse.server_process| grep -v grep | awk '{print $2, $50}'|grep -i "node$node_num"|awk '{print $1}'
+	
+	# Return value of the function is process id
+	return $my_pid
+}
+
+
+# Calling the function
+# Execution of the scripts calls the function with the command line argument
+# 1> /dev/null :- Suppresses the output from std_out
+
+ret_code=$(get_cassandra_ps_id_by_node_num $1) 1> /dev/null
+
+# If ret_code is empty then no process id is echoed to std-out
+if [ -z $ret_code ]
+then
+	echo "No Process Id found for the given node :- node$1"
+else
+	echo "Process id for node$1 is :- $ret_code"
+fi
