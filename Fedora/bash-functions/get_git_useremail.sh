1ed5d2ba09072a24a0970f76ac0c9fedc78a4d39
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-02 12:55:29 -0500

    get_git_useremail

---
+++ Fedora/bash-functions/get_git_useremail.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Declare the function name
+# This function fetches the configured email from git configuration file
+get_git_useremail()
+{
+	# Validate
+	# If git is installed
+	# and gitconfig file exists
+	if [ -n $(rpm -q git) ] && [ -f ~/.gitconfig ]
+		# Fetch the user email from git configuration file
+		then uemail=read grep -iF "email" ~/.gitconfig | xargs | cut -d '=' -f2 | xargs
+		# If the user email is not empty
+		if [ -n $uemail ]
+		then
+			# Return value of this function is :- Git User Email
+			return $uemail
+		else
+			# Return value of this function is :- Static text
+			return "Git-Configuration: User Email is not configured"
+		fi
+	fi
+}
+
+# Calling the function
+get_git_useremail
