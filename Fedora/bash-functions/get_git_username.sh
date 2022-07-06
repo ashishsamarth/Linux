fcf0b041b7bc2cead8a7d6d169a2e21bb3253a30
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-06 14:24:29 -0500

    get_git_username

--- Fedora/bash-functions/get_git_username.sh
+++ Fedora/bash-functions/get_git_username.sh
@@ -7,11 +7,11 @@ get_git_username()
 	# Validate
 	# if git is installed
 	# and gitconfig file exists
-	if [ -n $(rpm -q git) ] && [ -f ~/.gitconfig ]
+	if [[ -n $(rpm -q git) ]] && [[ -f ~/.gitconfig ]]
 		# fetch the username from the git configuration file
 		then uname=read grep -iF "name" ~/.gitconfig | xargs | cut -d '=' -f2 | xargs
 		# If the username value is not empty
-		if [ -n $uname ]
+		if [[ -n $uname ]]
 		then	
 			# Return value of this function is :- Git Username
 			return $uname
