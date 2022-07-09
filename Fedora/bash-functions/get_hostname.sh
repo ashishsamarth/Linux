c9b76527553eed40b1f8de94c247dc0c82091a8e
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-09 16:02:22 -0500

    get_hostname

--- Fedora/bash-functions/get_hostname.sh
+++ Fedora/bash-functions/get_hostname.sh
@@ -10,6 +10,17 @@ get_hostname()
     	
 	# Another way of executing the piped command in bash is by using command keyword
 	# my_hostname=command hostnamectl|grep -i hostname|cut -d ':' -f2|xargs
+
+	# AWK Equivalent of the above command
+	# awk '$1=$1' : Gets rid of the leading space just like xargs
+	# awk 'BEGIN {FS=": "}{print $2}	:- Modifies the default FieldSeparator (<space>) to ': '
+	# Advantage of this approach is that it allows multi character delimiter, while cut allows only single character delimiter	
+	# my_hostname=read hostnamectl | awk 'BEGIN {IGNORECASE=1} /hostname/ {print}' | awk '$1=$1' | awk 'BEGIN {FS=": "} {print $2}'
+
+	# SED + AWK Equivalent
+	# sed -n '/hostname/ Ip' : Performs a case insensitive search
+	# sed s'/^[[:space:]]//' : Gets rid of the leading space just like xargs
+	# my_hostname=read hostnamectl | sed -n '/hostname/ Ip' | sed 's/^[[:space:]]//' | awk 'BEGIN {FS=": "} {print $2}'
 	
 	# return value of the function is hostname
 	return $my_hostname
