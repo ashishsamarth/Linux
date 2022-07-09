2f2b2e755441475a4ba6d1e24465b45153dfc2b5
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-09 11:22:19 -0500

    get_architecture

--- Fedora/bash-functions/get_architecture.sh
+++ Fedora/bash-functions/get_architecture.sh
@@ -11,6 +11,13 @@ get_architecture()
 
 	# Another way of executing the piped command in bash is by using command keyword
 	# my_architecture=command hostnamectl|grep -i architecture|cut -d ':' -f2|xargs
+
+	# Another way of writing the same thing using sed and awk
+	# sed -n '/pattern/Ip' 	:- Performs case insensitive search
+	# awk '$1=$1 {print}'	:- Gets rid of the leading whitespaces
+	# awk 'BEGIN {FS=": "}{print $2}	:- Modifies the default FieldSeparator (<space>) to ': '
+	# Advantage of this approach is that it allows multi character delimiter, while cut allows only single character delimiter
+	#my_architecture=read hostnamectl | sed -n '/Architecture/Ip' |  awk '$1=$1 {print}' | awk 'BEGIN {FS=": "}{print $2}'
 	
 	# return value of the function is chip architecture
 	return $my_architecture
