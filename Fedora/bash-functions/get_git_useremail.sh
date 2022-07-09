504a4a9c6097104e4a82c9c2fed0db397db81975
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-09 11:33:08 -0500

    get_git_useremail

--- Fedora/bash-functions/get_git_useremail.sh
+++ Fedora/bash-functions/get_git_useremail.sh
@@ -10,6 +10,10 @@ get_git_useremail()
 	if [[ -n $(rpm -q git) ]] && [[ -f ~/.gitconfig ]]
 		# Fetch the user email from git configuration file
 		then uemail=read grep -iF "email" ~/.gitconfig | xargs | cut -d '=' -f2 | xargs
+
+		# AWK equivalent
+		# uemail=read awk 'BEGIN {IGNORECASE=1} /email/ {print}' ~/.gitconfig | awk '$1=$1' |awk 'BEGIN {FS=" = "} {print $2}'
+		
 		# If the user email is not empty
 		if [[ -n $uemail ]]
 		then
