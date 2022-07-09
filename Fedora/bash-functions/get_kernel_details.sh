783c25cb0654fa61ffa15d5249e3191e9de5ed84
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-09 16:04:33 -0500

    get_kernel_details

--- Fedora/bash-functions/get_kernel_details.sh
+++ Fedora/bash-functions/get_kernel_details.sh
@@ -11,7 +11,18 @@ get_kernel()
 
 	# Another way of executing the piped command in bash is by using command keyword
 	#my_kernel_details=command hostnamectl|grep -i kernel|cut -d ':' -f2|xargs
+
+	# AWK Equivalent
+	# awk '$1=$1' : Gets rid of the leading space just like xargs
+	# awk 'BEGIN {FS=": "}{print $2}	:- Modifies the default FieldSeparator (<space>) to ': '
+	# Advantage of this approach is that it allows multi character delimiter, while cut allows only single character delimiter	
+	# my_kernel=read hostnamectl | awk 'BEGIN {IGNORECASE=1} /kernel/ {print}' | awk '$1=$1' | awk 'BEGIN {FS=": "} {print $2}'
 	
+	# SED + AWK Equivalent
+	# sed -n '/hostname/ Ip' : Performs a case insensitive search
+	# sed s'/^[[:space:]]//' : Gets rid of the leading space just like xargs	
+	# my_kernel=read hostnamectl | sed -n '/kernel/ Ip' | sed 's/^[[:space:]]//' | awk 'BEGIN {FS=": "} {print $2}'
+
 	# return value of the function is kernal details
 	return $my_kernel_details
 }
