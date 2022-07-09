1a96c4b9746d6bf2320ee73672fcf40467bebe4b
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-09 17:24:25 -0500

    get_os

--- Fedora/bash-functions/get_os.sh
+++ Fedora/bash-functions/get_os.sh
@@ -11,6 +11,11 @@ get_os()
 	# Another way of executing the piped command in bash is by using command keyword
 	# my_os=command hostnamectl|grep -i operating|cut -d ':' -f2|xargs
 
+	# AWK Equivalent
+	# my_os=read hostnamectl | awk 'BEGIN {IGNORECASE=1} /operating/ {print}' | awk 'BEGIN {FS=": "}{print $2}'
+
+	#SED + AWK Equivalent
+	# my_os=read hostnamectl | sed -n '/operating/Ip' | awk 'BEGIN {FS=": "}{print $2}'
 	# return value of the function is operating system name
 	return $my_os
 }
