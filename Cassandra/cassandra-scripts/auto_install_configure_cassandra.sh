f7245001a05e077b9b11e2eea4abdd2a05a61dab
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-12 20:06:42 -0500

    auto_install_configure_cassandra

--- Cassandra/cassandra-scripts/auto_install_configure_cassandra.sh
+++ Cassandra/cassandra-scripts/auto_install_configure_cassandra.sh
@@ -2,3 +2,14 @@ trigger_owner=`whoami`
 tar_ball="dse-6.0.tar.gz"
 if [[ $trigger_owner == 'root' ]]
 
+then
+    read -p 'Enter the number of nodes you wish to create:- ' num_node
+    if [[ "$num_node" =~ ^[0-9]+$ ]] && [[ $num_node > 0 ]]
+    then
+        echo "" 
+        echo "stage#1 : Download Datastax Cassandra"
+        cd /tmp && echo "Downloading Datastax Cassandra version 6.0.0"
+        wget -q https://downloads.datastax.com/enterprise/$tar_ball && echo "Tar-Ball: Downloaded Successfully"
+        sleep 5
+        tar -xf $tar_ball && echo "Tar-Ball: Uncompressed successfully in `pwd`"
+        rm -rf $tar_ball && echo "Tar-Ball: Deleted from `pwd`"
