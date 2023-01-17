0abe87a3fd5ab293be766012916758c0b4ae61db
Ashish Samarth <mrashishsamarth@gmail.com>
2023-01-16 19:17:06 -0600

    get_cassandra_node_ps_ids_by_node_num

--- Cassandra/cassandra-scripts/get_cassandra_node_ps_ids_by_node_num.sh
+++ Cassandra/cassandra-scripts/get_cassandra_node_ps_ids_by_node_num.sh
@@ -3,17 +3,17 @@
 # Declare function name
 get_cassandra_ps_id_by_node_num()
 {
-	# Positional Argument of node number is assiged to variable
-	node_num=$1
-	# Always make sure read is present
-	# read allows to run and return the output of the command to the variable
-	# $2 gets the pid for the node
-	# $50 helps identifies the node number
-	# final output is the process id of the node number provided by user as cmd line argument
-	my_pid=read ps -aux | grep Ddse.server_process| grep -v grep | awk '{print $2, $50}'|grep -i "node$node_num"|awk '{print $1}'
-	
-	# Return value of the function is process id
-	return $my_pid
+        # Positional Argument of node number is assiged to variable
+        node_num=$1
+        # Always make sure read is present
+        # read allows to run and return the output of the command to the variable
+        # $2 gets the pid for the node
+        # $50 helps identifies the node number
+        # final output is the process id of the node number provided by user as cmd line argument
+        my_pid=read ps -aux | grep Ddse.server_process| grep -v grep | awk '{print $2, $50}'|grep -i "node$node_num"|awk '{print $1}'
+
+        # Return value of the function is process id
+        return $my_pid
 }
 
 
@@ -27,7 +27,7 @@ ret_code=$(get_cassandra_ps_id_by_node_num $1) 1> /dev/null
 # If ret_code is empty then no process id is echoed to std-out
 if [[ -z $ret_code ]]
 then
-	echo "No Process Id found for the given node :- node$1"
+        echo "No Process Id found for the given node :- node$1"
 else
-	echo "Process id for node$1 is :- $ret_code"
+        echo "Process id for node$1 is :- $ret_code"
 fi
